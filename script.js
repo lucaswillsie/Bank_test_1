a3e0639695c724f842e95c19abf08e7b6ba05347
lucaswillsie <60862306+lucaswillsie@users.noreply.github.com>
2021-11-08 08:55:43 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function username()
+{
+var input = document.getElementById("userInput");
+alert(input);
+}
+
+function password()
+{
+var input = document.getElementById("userInput");
+alert(input);
+}
